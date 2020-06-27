e47fa2321e7e072b6c6e2f8110c589bd5338081a
Samuel212584 <samuel212584@gmail.com>
2020-06-27 20:30:00 -0300

    Sign Up page implementation

---
+++ src/pages/SignUp/index.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { Form } from '@unform/web';
+
+import { FiArrowLeft, FiMail, FiUser, FiLock } from 'react-icons/fi';
+import logoImg from '../../assets/logo.svg';
+import { Container, Content, Background } from './styles';
+
+import Button from '../../components/button';
+import Input from '../../components/input';
+
+const SignUp: React.FC = () => {
+  function handleSubmit(data: object): void {
+    console.log(data);
+  }
+
+  return (
+    <Container>
+      <Background />
+
+      <Content>
+        <img src={logoImg} alt="GoBarber" />
+
+        <Form onSubmit={handleSubmit}>
+          <h1>Faça seu Logon</h1>
+
+          <Input name="name" icon={FiUser} placeholder="Nome" />
+          <Input name="email" icon={FiMail} placeholder="E-mail" />
+          <Input
+            name="password"
+            icon={FiLock}
+            type="password"
+            placeholder="Senha"
+          />
+
+          <Button type="submit">Cadastrar</Button>
+        </Form>
+
+        <a href="login">
+          <FiArrowLeft />
+          Voltar para logon
+        </a>
+      </Content>
+    </Container>
+  );
+};
+
+export default SignUp;
